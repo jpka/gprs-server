72d1dbc64a4d624f2e8e99a301ed9db9752161a5
Juan Kaniefsky <jpkaniefsky@gmail.com>
2018-05-16 19:31:21 -0300

    handle status reports and other minor changes

--- test/general.js
+++ test/general.js
@@ -63,6 +63,27 @@ test.cb("should respond to heartbeat", t => {
     t.context.heartbeat();
 });
 
+test.cb("handles status reports", t => {
+    t.context.tracker.on("report", data => {
+        t.deepEqual(data.data, {
+            imei: "353990030327618",
+            date: new Date("2009-11-17T02:09:28.000Z"),
+            latitude: 22.537222,
+            longitude: 114.020948
+        });
+        t.end();
+    });
+    t.context.socket.write("PA$353990030327618$D#091117#020928#1#22.537222#114.020948#0.00#0.0#42.1#4183#011#1#101#Wsz-wl001#B0101940#C+3.0,-5.0,+2.0$AP");
+});
+
+test.cb("triggers error and displays original payload when failed to parse a message", t => {
+    t.context.tracker.on("error", (error, buffer) => {
+        t.is(buffer.toString(), "21321sdsad/(&%&%$/$)0x8766");
+        t.end();
+    });
+    t.context.socket.write("21321sdsad/(&%&%$/$)0x8766");
+});
+
 // test.cb("queries gps location correctly", t => {
 //     t.context.tracker.requestReport("gps", (err, data) => {
 //         if (err) {
