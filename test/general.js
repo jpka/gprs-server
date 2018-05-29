d8673e740e1e9a1fcb7c69a312947f3918ca887e
Juan Kaniefsky <jpkaniefsky@gmail.com>
2018-05-28 23:21:00 -0300

    adapted to new protocol status report and heartbeat

--- test/general.js
+++ test/general.js
@@ -17,12 +17,12 @@ test.beforeEach.cb(t => {
     t.context.socket = new Socket();
     t.context.socket.connect(20180);
 
-    t.context.login = (cb) => t.context.socket.write("PA$353990030327618$20$AP", "utf8", () => {
-        if (cb) {
-            t.context.socket.on("data", data => data.toString() === "PA$353990030327618$20$AP" ? cb() : 0);
-        }
-    });
-    t.context.heartbeat = (cb) => t.context.socket.write("PA$353990030327618$22$AP", "utf8", cb);
+    // t.context.login = (cb) => t.context.socket.write("PA$353990030327618$20$AP", "utf8", () => {
+    //     if (cb) {
+    //         t.context.socket.on("data", data => data.toString() === "PA$353990030327618$20$AP" ? cb() : 0);
+    //     }
+    // });
+    t.context.heartbeat = (cb) => t.context.socket.write("HB$353990030327618#22", "utf8", cb);
 });
 
 test.afterEach.cb(t => {
@@ -35,21 +35,21 @@ test.afterEach.cb(t => {
     t.context.tracker.disconnect();
 });
 
-test.cb("recognizes device login", t => {
-    t.context.tracker.on("login", function() {
-        t.pass();
-        t.end();
-    });
-    t.context.login();
-});
+// test.cb("recognizes device login", t => {
+//     t.context.tracker.on("login", function() {
+//         t.pass();
+//         t.end();
+//     });
+//     t.context.login();
+// });
 
-test.cb("responds to device login", t => {
-    t.context.socket.on("data", data => {
-        t.is(data.toString(), "PA$353990030327618$20$AP");
-        t.end();
-    }); 
-    t.context.login();
-});
+// test.cb("responds to device login", t => {
+//     t.context.socket.on("data", data => {
+//         t.is(data.toString(), "PA$353990030327618$20$AP");
+//         t.end();
+//     }); 
+//     t.context.login();
+// });
 
 test.cb("recognizes heartbeat", t => {
     t.context.tracker.on("heartbeat", () => {
@@ -61,7 +61,7 @@ test.cb("recognizes heartbeat", t => {
 
 test.cb("responds to heartbeat", t => {
     t.context.socket.on("data", function(data){
-        t.is(data.toString(), "PA$353990030327618$22$AP");
+        t.is(data.toString(), "HB$353990030327618#21");
         t.end();
     });
     t.context.heartbeat();
@@ -71,52 +71,65 @@ test.cb("handles status reports", t => {
     t.context.tracker.on("report", data => {
         t.deepEqual(data.data, {
             imei: "353990030327618",
-            date: new Date("2009-11-17T02:09:28.000Z"),
-            latitude: 22.537222,
-            longitude: 114.020948
+            date: new Date("2018-10-01T13:30:15.000Z"),
+            latitude: 22.64611,
+            longitude: 113.82682
         });
         t.end();
     });
-    t.context.socket.write("PA$353990030327618$D#091117#020928#1#22.537222#114.020948#0.00#0.0#42.1#4183#011#1#101#Wsz-wl001#B0101940#C+3.0,-5.0,+2.0$AP");
+    t.context.socket.write("PA$353990030327618#011018#133015#+22.64611#+113.82682#120#115#085#38#121#25#8#121812");
 });
 
-test.cb("triggers error and displays original payload when failed to parse a message", t => {
-    t.context.tracker.on("error", (error, buffer) => {
-        t.is(buffer.toString(), "21321sdsad/(&%&%$/$)0x8766");
-        t.end();
+test.cb("handles status reports with invalid data", t => {
+  t.context.tracker.on("report", data => {
+    t.deepEqual(data.data, {
+        imei: "130839038110",
+        date: null,
+        latitude: null,
+        longitude: null
     });
-    t.context.socket.write("21321sdsad/(&%&%$/$)0x8766");
+    t.end();
+  });
+  t.context.socket.write("PA$130839038110#DDMMAA#171229#Latitud#Longitud#999#360#9999#39#122#12#XXXX");
 });
 
-test.cb("sets up timed reports and report success", t => {
-    t.context.login(() => {
-        t.context.socket.on("data", data => {
-            t.is(data.toString(), "PA$2$02$1#00000A$AP");
-            t.context.socket.write("PA$353990030327618$2$02$AP");
-        });
-        t.context.tracker.setReportTimeInterval("00:00:10", (err, result) => {
-            if (err) throw err;
+// test.cb("triggers error and displays original payload when failed to parse a message", t => {
+//     t.context.tracker.on("error", (error, buffer) => {
+//         t.is(buffer.toString(), "21321sdsad/(&%&%$/$)0x8766");
+//         t.end();
+//     });
+//     t.context.socket.write("21321sdsad/(&%&%$/$)0x8766");
+// });
+
+// test.cb("sets up timed reports and report success", t => {
+//     t.context.login(() => {
+//         t.context.socket.on("data", data => {
+//             t.is(data.toString(), "PA$2$02$1#00000A$AP");
+//             t.context.socket.write("PA$353990030327618$2$02$AP");
+//         });
+//         t.context.tracker.setReportTimeInterval("00:00:10", (err, result) => {
+//             if (err) throw err;
             
-            t.is(result, "success");
-            t.end();
-        });
-    });
-});
+//             t.is(result, "success");
+//             t.end();
+//         });
+//     });
+// });
 
-test.cb("disables timed reports and report success", t => {
-    t.context.login(() => {
-        t.context.socket.on("data", data => {
-            t.is(data.toString(), "PA$2$02$0#000000$AP");
-            t.context.socket.write("PA$353990030327618$2$02$AP");
-        });
-        t.context.tracker.setReportTimeInterval("00:00:00", (err, result) => {
-            if (err) throw err;
+// test.cb("disables timed reports and report success", t => {
+//     t.context.login(() => {
+//         t.context.socket.on("data", data => {
+//             t.is(data.toString(), "PA$2$02$0#000000$AP");
+//             t.context.socket.write("PA$353990030327618$2$02$AP");
+//         });
+//         t.context.tracker.setReportTimeInterval("00:00:00", (err, result) => {
+//             if (err) throw err;
             
-            t.is(result, "success");
-            t.end();
-        });
-    });
-});
+//             t.is(result, "success");
+//             t.end();
+//         });
+//     });
+// });
 
 // test.cb("queries gps location correctly", t => {
 //     t.context.tracker.requestReport("gps", (err, data) => {
